bcb9f203ffdfe2ccf77463a3e3d3036ceb4a3bd6
ra21708452 <ra21708452@AN0700114077504.dinfor.uniceub>
2017-09-29 21:03:36 -0300

    Commit Com a 1 e 2 Terminada.

---
+++ src/Principal.java
@@ -0,0 +1,16 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// Josue Petrucci RA:21708452
+		BDSimulado bd = new BDSimulado();
+		
+		System.out.println(bd.getInfoCliente("Evandro da Silva"));
+		System.out.println(bd.ligacoesPorCliente("Evandro da Silva"));
+		System.out.println(bd.ligacoesPorUF("DF"));
+		System.out.println(bd.ligacoesUFDiferente());
+		
+
+	}
+
+}
